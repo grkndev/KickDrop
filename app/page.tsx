1c59768d88d3b3e8796075edf23fe3df0f2886d4
grkndev <gweepcreative@gmail.com>
2025-10-30 15:17:23 +0200

    subs period & chat block

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
 import {
   Card,
   CardAction,
@@ -8,19 +10,43 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldLabel,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Separator } from "@/components/ui/separator";
 import { Slider } from "@/components/ui/slider";
 import { Switch } from "@/components/ui/switch";
+import { Minus, Plus } from "lucide-react";
 import React from "react";
 
 export default function Home() {
+  const [subsPeriod, setSubsPeriod] = React.useState(8);
+
+  const handleSubsAdjustment = React.useCallback((adjustment: number) => {
+    setSubsPeriod((prevCount) =>
+      Math.max(1, Math.min(99, prevCount + adjustment))
+    );
+  }, []);
+
+  const handleSubsInputChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = parseInt(e.target.value, 10);
+      if (!isNaN(value) && value >= 1 && value <= 99) {
+        setSubsPeriod(value);
+      }
+    },
+    []
+  );
   return (
     <div className="h-full w-full flex flex-col items-center justify-center p-4 gap-4">
       <Header />
-      <div className="h-full w-full flex flex-row gap-4">
+      <div className="h-full w-full grid grid-cols-4 gap-4">
         <Card className="w-full h-full">
           <CardHeader className="flex">
             <CardTitle className="text-2xl font-bold">Settings</CardTitle>
@@ -66,7 +92,7 @@ export default function Home() {
                 <Separator className="bg-kick" />
 
                 {/* SWITCHS */}
-                <div className="max-w-1/2 flex flex-col gap-2">
+                <div className="flex flex-col gap-2">
                   <div className="flex justify-between">
                     <Label className="text-lg" htmlFor="subs-only">
                       Subscribers only
@@ -111,18 +137,50 @@ export default function Home() {
                 <Separator className="bg-kick" />
 
                 {/* SUBS Settings */}
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center gap-4">
-                    <h3 className="text-lg font-medium">Subs period</h3>
-                    <Input
-                      placeholder="0"
-                      type="number"
-                      inputMode="numeric"
-                      className="max-w-16 text-center"
-                    />
-                    <h3 className="text-lg font-medium">month(s)</h3>
+                <div className="flex flex-col gap-4">
+                  <div className="flex items-center justify-between gap-4">
+                    <Field orientation="horizontal">
+                      <FieldContent>
+                        <FieldLabel htmlFor="number-of-gpus-f6l">
+                          Subs period
+                        </FieldLabel>
+                        <FieldDescription>month(s) and above</FieldDescription>
+                      </FieldContent>
+                      <ButtonGroup>
+                      <Button
+                          variant="outline"
+                          size="icon-sm"
+                          type="button"
+                          aria-label="Decrement"
+                          onClick={() => handleSubsAdjustment(-1)}
+                          disabled={subsPeriod <= 1}
+                        >
+                          <Minus />
+                        </Button>
+                        <Input
+                          id="number-of-gpus-f6l"
+                          value={subsPeriod}
+                          onChange={handleSubsInputChange}
+                          size={3}
+                          className="h-8 !w-14 text-center"
+                          maxLength={3}
+                        />
+                       
+                        <Button
+                          variant="outline"
+                          size="icon-sm"
+                          type="button"
+                          aria-label="Increment"
+                          onClick={() => handleSubsAdjustment(1)}
+                          disabled={subsPeriod >= 99}
+                        >
+                          <Plus />
+                        </Button>
+                      </ButtonGroup>
+                    </Field>
                   </div>
-                  <div className="flex flex-col gap-2">
+
+                  <div className="flex flex-col gap-0">
                     <h3 className="text-lg font-medium">Subs luck</h3>
                     <div className="flex items-center gap-4">
                       <Slider />
@@ -133,19 +191,27 @@ export default function Home() {
               </div>
 
               <div className="flex flex-col gap-2">
-                <Button
-                  className="w-full py-6 bg-kick hover:bg-kick/75 text-base font-semibold"
-                 
-                >
+                <Button className="w-full py-6 bg-kick hover:bg-kick/75 text-base font-semibold">
                   Pick Winner
                 </Button>
                 <div className=" flex flex-row gap-2">
-                  <Button className="flex-1 py-6 bg-[#404040] hover:bg-[#404040]/75 text-white font-semibold">Clear Participants</Button>
-                  <Button className="flex-1 py-6 bg-[#404040] hover:bg-[#404040]/75 text-white font-semibold">Clear Winners</Button>
+                  <Button className="flex-1 py-6 bg-[#404040] hover:bg-[#404040]/75 text-white font-semibold">
+                    Clear Participants
+                  </Button>
+                  <Button className="flex-1 py-6 bg-[#404040] hover:bg-[#404040]/75 text-white font-semibold">
+                    Clear Winners
+                  </Button>
                 </div>
               </div>
             </div>
           </CardContent>
+        </Card>
+
+        <Card className="w-full h-full">
+          <CardHeader className="flex flex-row items-center justify-between">
+            <CardTitle className="text-2xl font-bold">Chat</CardTitle>
+          </CardHeader>
+          <Separator className="bg-kick" />
         </Card>
 
         <Card className="w-full h-full">
