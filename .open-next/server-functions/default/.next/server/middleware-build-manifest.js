4672225f6c1f56ed39bef0ae48b2e13728303d86
grkndev <gweepcreative@gmail.com>
2025-11-02 16:01:09 +0200

    workers

---
+++ .open-next/server-functions/default/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/5b1006cedf77a80b.js",
+    "static/chunks/af984078aa3a48ea.js",
+    "static/chunks/e61deb4515d9e085.js",
+    "static/chunks/turbopack-2778cf8ff5954143.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TknWwzV25aCobk3HgNMTcvklhEHHwWZOsZH13884BLg=\"\n}"
